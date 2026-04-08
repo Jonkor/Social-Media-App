9bbb7fcb90dddd3f0a46456e79919017e912f12d
Jonkor <juan.kucho@hotmail.com>
2026-04-08 01:10:16 -0700

    setup form action

--- app/new-post/page.tsx
+++ app/new-post/page.tsx
@@ -1,8 +1,16 @@
 export default function NewPostPage() {
+  function createPost(formData: FormData) {
+    const title = formData.get('title') as string;
+    const image = formData.get('image') as File;
+    const content = formData.get('content') as string;
+
+    console.log(title, image, content);
+  }
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form>
+      <form action={createPost}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
@@ -18,7 +26,7 @@ export default function NewPostPage() {
         </p>
         <p className="form-control">
           <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
+          <textarea id="content" name="content" rows={5} />
         </p>
         <p className="form-actions">
           <button type="reset">Reset</button>
